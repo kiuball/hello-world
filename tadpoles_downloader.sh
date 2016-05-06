480eafadceda598a54b0ddef2d52a6352adceeed
kiuball <sheldon.kiu@gmail.com>
2016-05-06 15:12:44 -0400

    Allow multiple input files
    using'
    for input in "$@"
    do
        #stuff on $input instead of $1
    done

--- tadpoles_downloader.sh
+++ tadpoles_downloader.sh
@@ -8,10 +8,6 @@
 #wget <url> -O <new_file_name> 
 #wget https://www.tadpoles.com/m/p/ZfShwgoKAxpCEC8NkTuctE?d=t -O filename.jpg
 ######################################################################
-
-declare -i count
-count=1
-######################################################################
 # Logic
 # read each line from inputfile (arg1)
 # expect inputfile name to be <child>-<yyyymmdd>
@@ -19,27 +15,32 @@ count=1
 # rename file to naming convention:
 #   Kiddie_Academy_<child><yyyymmdd><file#>.<jpg|mp4>
 ######################################################################
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    echo "[CMD] wget $line?d=t -O $$.tmp"
-    wget $line?d=t -O $$.tmp
-    # Add case statement to check file= jpg or iso #file $$.tmp
-    TYPE=`file $$.tmp | awk '{ print $2 }' `
-    case "$TYPE" in
-    JPEG)
-        echo "TYPE is a jpg"
-        echo "[CMD] mv $$.tmp Kiddie_Academy_$1_$count.jpg"
-        mv $$.tmp Kiddie_Academy_$1_$count.jpg
-        ;;
-    ISO)
-        echo "TYPE is a mp4"
-        echo "[CMD] mv $$.tmp Kiddie_Academy_$1_$count.mp4"
-        mv $$.tmp Kiddie_Academy_$1_$count.mp4
-        ;;
-    *)
-        echo "[ERROR] file type is not JPEG/ISO"
-        echo "[ERROR] will not act on this file: $$.tmp"
-        echo "[ERROR] skipping"
-        ;;
-    esac
-    count=$count+1
-done < "$1"
+declare -i count #index number per child per day
+for input in "$@"
+do
+    count=1 #[Re]Set index number per child
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        echo "[CMD] wget $line?d=t -O $$.tmp"
+        wget $line?d=t -O $$.tmp
+        # Add case statement to check file= jpg or iso #file $$.tmp
+        TYPE=`file $$.tmp | awk '{ print $2 }' `
+        case "$TYPE" in
+        JPEG)
+            echo "TYPE is a jpg"
+            echo "[CMD] mv $$.tmp Kiddie_Academy_${input}_${count}.jpg"
+            mv $$.tmp Kiddie_Academy_${input}_${count}.jpg
+            ;;
+        ISO)
+            echo "TYPE is a mp4"
+            echo "[CMD] mv $$.tmp Kiddie_Academy_${input}_${count}.mp4"
+            mv $$.tmp Kiddie_Academy_${input}_${count}.mp4
+            ;;
+        *)
+            echo "[ERROR] file type is not JPEG/ISO"
+            echo "[ERROR] will not act on this file: $$.tmp"
+            echo "[ERROR] skipping"
+            ;;
+        esac
+        count=$count+1
+    done < "$input"
+done
